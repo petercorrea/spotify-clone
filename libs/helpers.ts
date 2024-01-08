d546904c22227b984e24ac8cc6a9e4c027189127
Peter Correa <pmcorrea@me.com>
2024-01-07 22:03:36 -0500

    updated return url for stripe

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -2,8 +2,8 @@ import { Price } from '@/types/types';
 
 export const getURL = () => {
   let url =
-    process.env.NEXT_PUBLIC_SITE_URL ??
     process.env.NEXT_PUBLIC_VERCEL_URL ??
+    process.env.NEXT_PUBLIC_SITE_URL ??
     'http://localhost:3000/';
 
   url = url.includes('http') ? url : `https://${url}`;
